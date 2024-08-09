b95548ab50ea355858bee8ab3cf05bfa57f2c009
alwinsDen <jsmadesimple@gmail.com>
2024-08-09 12:44:59 +0530

    test-test

--- src/pages/exp1/index.jsx
+++ src/pages/exp1/index.jsx
@@ -2,6 +2,8 @@ import img1 from "../../assets/img.png";
 import img2 from "../../assets/img_1.png";
 import img3 from "../../assets/img_2.png";
 import PyCharm from "../assets/pycharm.svg";
+import Rider from "../assets/rider.gif";
+import Idea from "../assets/idea.svg";
 import "./index.css";
 import { useEffect, useRef } from "react";
 const Exp1 = () => {
@@ -10,6 +12,18 @@ const Exp1 = () => {
   const lastElement = useRef(null);
   const firstRef = useRef(null);
   let mainElementInView = false;
+
+  // here is the list of all text elemental refs
+  const textElement1 = useRef(null);
+  const textElement2 = useRef(null);
+  const textElement3 = useRef(null);
+  const textElement4 = useRef(null);
+  const elementalControl = {
+    textElement1: PyCharm,
+    textElement2: Rider,
+    textElement3: Idea,
+    textElement4: Rider,
+  };
   useEffect(() => {
     let lastScrollY = window.scrollY;
     let scrollDirection = "down";
@@ -20,6 +34,22 @@ const Exp1 = () => {
     });
     window.addEventListener("load", () => {
       loaded = true;
+    });
+    const allElnts = [textElement1, textElement2, textElement3, textElement4];
+    // Observer to watch the Text Elements
+    const textElObserver = new IntersectionObserver(
+      ([entry]) => {
+        if (!loaded) return;
+        console.log("THIS IS THE ELEMTN ID", entry.target.id);
+        imageRef.current.querySelector("img").src =
+          elementalControl[entry.target.id];
+      },
+      {
+        threshold: 1,
+      },
+    );
+    allElnts.forEach((elms) => {
+      textElObserver.observe(elms.current);
     });
     const observer = new IntersectionObserver(
       ([entry]) => {
@@ -77,6 +107,23 @@ const Exp1 = () => {
         scrollDirection = window.scrollY > lastScrollY ? "down" : "up";
         lastScrollY = window.scrollY;
       });
+      window.removeEventListener("scroll", () => {
+        if (window.scrollY < 4200 && scrollDirection === "up") {
+          if (mainElementInView === false) {
+            imageRef.current.style.position = "fixed";
+            imageRef.current.style.top = "100px";
+          }
+        }
+        if (window.scrollY > 4200) {
+          imageRef.current.style.position = "";
+          lastElement.current.appendChild(imageRef.current);
+        }
+      });
+      window.removeEventListener("load", () => {
+        loaded = true;
+      });
+      observer.unobserve(mainRef.current);
+      lastObserver.unobserve(lastElement.current);
     };
   }, []);
   return (
@@ -123,9 +170,10 @@ const Exp1 = () => {
                 flexDirection: "column",
                 gap: "20px",
                 width: "500px",
-                marginLeft: "50px",
                 marginTop: "50px",
               }}
+              id={"textElement1"}
+              ref={textElement1}
             >
               <p style={{ fontSize: "38px", fontWeight: 600 }}>
                 Ready for actual use right out of the box
@@ -145,7 +193,7 @@ const Exp1 = () => {
                   position: "relative",
                 }}
               >
-                <img src={PyCharm} />
+                <img src={PyCharm} height={"100%"} />
               </div>
             </div>
           </div>
@@ -160,9 +208,10 @@ const Exp1 = () => {
                 flexDirection: "column",
                 gap: "20px",
                 width: "500px",
-                marginLeft: "50px",
                 marginTop: "50px",
               }}
+              id={"textElement2"}
+              ref={textElement2}
             >
               <p style={{ fontSize: "38px", fontWeight: 600 }}>
                 Complex tasks become easy
@@ -186,9 +235,10 @@ const Exp1 = () => {
                 flexDirection: "column",
                 gap: "20px",
                 width: "500px",
-                marginLeft: "50px",
                 marginTop: "50px",
               }}
+              id={"textElement3"}
+              ref={textElement3}
             >
               <p style={{ fontSize: "38px", fontWeight: 600 }}>
                 Customizable and extendable
@@ -212,9 +262,10 @@ const Exp1 = () => {
                 flexDirection: "column",
                 gap: "20px",
                 width: "500px",
-                marginLeft: "50px",
                 marginTop: "50px",
               }}
+              id={"textElement4"}
+              ref={textElement4}
             >
               <p style={{ fontSize: "38px", fontWeight: 600 }}>
                 Built-in tools
